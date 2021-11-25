82b171757db0f11538e31ea31ddafffcf73cf9e7
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-11-25 20:26:47 +0200

    Add initial Project

---
+++ GHSearch/GHSearchUITests/GHSearchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GHSearchUITestsLaunchTests.swift
+//  GHSearchUITests
+//
+//  Created by Cédric Bahirwe on 25/11/2021.
+//
+
+import XCTest
+
+class GHSearchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
