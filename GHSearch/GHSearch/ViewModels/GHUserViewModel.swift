a50ae5890a2f046229d8ff94cc7972e1a8bc63cc
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-11-27 18:00:52 +0200

    Addding Unit Tests

---
+++ GHSearch/GHSearch/ViewModels/GHUserViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  GHUserViewModel.swift
+//  GHSearch
+//
+//  Created by Cédric Bahirwe on 27/11/2021.
+//
+
+import Foundation
+import RxSwift
+
+
+class GHUserViewModel {
+    
+    var user: User!
+    
+    
+    let showError = PublishSubject<Any>()
+    
+    let disposeBag = DisposeBag()
+    
+    private let loadInProgress = false
+    
+}
