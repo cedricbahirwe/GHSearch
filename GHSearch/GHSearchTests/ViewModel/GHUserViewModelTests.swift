9f93ed25215aed0bf858ba51b23947531ceb5466
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-11-30 09:40:36 +0200

    Add MockAppServer

--- GHSearch/GHSearchTests/ViewModel/GHUserViewModelTests.swift
+++ GHSearch/GHSearchTests/ViewModel/GHUserViewModelTests.swift
@@ -9,3 +9,117 @@ import XCTest
 import RxSwift
 @testable import GHSearch
 
+class GHUserViewModelTests: XCTestCase {
+    
+//    func testAddFriendSuccess() {
+//        let disposeBag = DisposeBag()
+//        let appServerClient = MockAppServerClient()
+//        appServerClient.postUserResult = .success(payload: ())
+//
+//        let viewModel = AddFriendViewModel(appServerClient: appServerClient)
+//
+//        let mockFriend = Friend.with()
+//        viewModel.firstname.accept(mockFriend.firstname)
+//        viewModel.lastname.accept(mockFriend.lastname)
+//        viewModel.phonenumber.accept(mockFriend.phonenumber)
+//
+//        let expectNavigateCall = expectation(description: "Navigate back is called")
+//
+//        viewModel.onNavigateBack.asObservable().debug().subscribe(
+//            onNext: { _ in
+//                expectNavigateCall.fulfill()
+//            }
+//        ).disposed(by: disposeBag)
+//
+//        viewModel.submitButtonTapped.onNext(())
+//
+//        wait(for: [expectNavigateCall], timeout: 0.1)
+//    }
+    
+//    func testAddFriendFailure() {
+//        let disposeBag = DisposeBag()
+//        let appServerClient = MockAppServerClient()
+//        appServerClient.postUserResult = .failure(AppServerClient.PostFriendFailureReason(rawValue: 401))
+//
+//        let viewModel = AddFriendViewModel(appServerClient: appServerClient)
+//
+//        let mockFriend = Friend.with()
+//        viewModel.firstname.accept(mockFriend.firstname)
+//        viewModel.lastname.accept(mockFriend.lastname)
+//        viewModel.phonenumber.accept(mockFriend.phonenumber)
+//
+//        let expectErrorShown = expectation(description: "OnShowError is called")
+//
+//        viewModel.onShowError.asObservable().subscribe(
+//            onNext: { singleButtonAlert in
+//                expectErrorShown.fulfill()
+//            }
+//        ).disposed(by: disposeBag)
+//
+//        viewModel.submitButtonTapped.onNext(())
+//
+//        wait(for: [expectErrorShown], timeout: 0.1)
+//    }
+    
+//    func testValidateInputSuccess() {
+//        let disposeBag = DisposeBag()
+//        let mockFriend = Friend.with()
+//        let appServerClient = MockAppServerClient()
+//
+//        let viewModel = AddFriendViewModel(appServerClient: appServerClient)
+//        viewModel.firstname.accept(mockFriend.firstname)
+//        viewModel.lastname.accept(mockFriend.lastname)
+//        viewModel.phonenumber.accept(mockFriend.phonenumber)
+//
+//        let expectUpdateSubmitButtonStateCall = expectation(description: "updateSubmitButtonState is called")
+//
+//        viewModel.submitButtonEnabled.subscribe(
+//            onNext: { state in
+//                guard state else { return }
+//
+//                expectUpdateSubmitButtonStateCall.fulfill()
+//            }
+//        ).disposed(by: disposeBag)
+//
+//        wait(for: [expectUpdateSubmitButtonStateCall], timeout: 0.1)
+//    }
+    
+}
+
+
+private final class MockAppServerClient: NetworkingManager {
+    var getFollowersResult: Result<[Follower], GHSearchError>?
+    var getUserResult: Result<User, GHSearchError>?
+    
+    
+    override func getFollowers(for username: String, page: Int) -> Observable<[Follower]> {
+        
+        return Observable.create { observer in
+            switch self.getFollowersResult {
+            case .success(let followers):
+                observer.onNext(followers)
+            case .failure(let error):
+                observer.onError(error)
+            case .none:
+                observer.onError(GHSearchError.notFound)
+            }
+
+            return Disposables.create()
+        }
+    }
+
+    override func getUserInfo(for username: String) -> Observable<User> {
+        return Observable.create { observer in
+            switch self.getUserResult {
+            case .success(let user):
+                observer.onNext(user)
+            case .failure(let error):
+                observer.onError(error)
+            case .none:
+                observer.onError(GHSearchError.notFound)
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
